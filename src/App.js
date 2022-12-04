1d8eae8623c3b170d94a00e2f374362922cd279c
Troy Mathis <troy.mathis00@gmail.com>
2022-12-04 16:12:44 -0600

    project completed :)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-
 import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+import {
+  decrement,
+  increment,
+  reset,
+  logIn,
+  logOut,
+} from "./redux/actions/index";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Counter</h1>
+        <h3>{counter}</h3>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(reset())}>Reset</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+
+        <h2>User Login</h2>
+        <button onClick={() => dispatch(logIn())}>LogIn</button>
+        <button onClick={() => dispatch(logOut())}>LogOut</button>
+
+        {auth ? (
+          <div>
+            <p>
+              As a user, I'm Logged In!
+            </p>
+          </div>
+        ) : (
+          <div>
+            <p>Not in the mainframe :/ </p>
+          </div>
+        )}
     </div>
   );
 }
